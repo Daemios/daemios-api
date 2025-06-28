6af06913cc0778a41a2f9a0414d6467aea969224
Michael Ryan <mryanwebdev@gmail.com>
2025-06-28 17:33:09 -0500

    Refactor arena and world setup

---
+++ src/lib/setupArenaWorld.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import generate from './generate.js';
+import wss from './socket.js';
+
+export default async function setupArenaWorld(app) {
+  const prisma = new PrismaClient();
+
+  // Initialize arena data
+  app.locals.arena = {};
+  try {
+    const last = await prisma.arena_history.findFirst({
+      orderBy: { last_active: 'desc' },
+    });
+    if (last) {
+      app.locals.arena.terrain = generate.arena.simplexTerrain(last.size, last.seed);
+      wss.send('arena', app.locals.arena);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+
+  // Initialize world data
+  app.locals.world = {
+    size: 256,
+    seed: 'einstein2',
+  };
+  wss.send('world', app.locals.world);
+}
