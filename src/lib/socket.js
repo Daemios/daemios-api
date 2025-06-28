ced969fb6bc81fdc2ee42dcf5955f16ab43bd29d
Michael Ryan <mryanwebdev@gmail.com>
2025-06-28 17:26:11 -0500

    fix ws import and broadcast check

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({ port: 3001 });
 
@@ -13,7 +13,7 @@ wss.on('connection', (socket) => {
 wss.send = function broadcast(type, body) {
   const message = JSON.stringify({ type, body });
   wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.WebSocket.OPEN) {
+    if (client.readyState === WebSocket.OPEN) {
       client.send(message);
     }
   });
